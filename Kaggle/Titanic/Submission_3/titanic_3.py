35c6709cdaa09fcc3ecf63fd9307785224e86ac2
Mustafa Cengiz <mustafacengiz98@gmail.com>
2022-07-17 22:11:53 +0300

    .

--- Kaggle/Titanic/Submission_3/titanic_3.py
+++ Kaggle/Titanic/Submission_3/titanic_3.py
@@ -104,6 +104,7 @@ comp = output_f.compare(compare_with_local_best) # 52 different values, 12.44019
 
 # i added this line so i get an extra commit on github lol
 # same lol
+# same lol 17/07/2022
  
 ###########
 # Spoiler #
